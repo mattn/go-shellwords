efcefb0c14b8e210d49fcb836b73fa303afe8c87
mattn <mattn.jp@gmail.com>
2014-06-19 12:04:25 +0900

    First import

---
+++ shellwords.go
@@ -0,0 +1,82 @@
+package shellwords
+
+import (
+	"errors"
+	"strings"
+)
+
+func isSpace(r rune) bool {
+	switch r {
+	case ' ', '\t', '\r', '\n':
+		return true
+	}
+	return false
+}
+
+func Parse(line string) ([]string, error) {
+	line = strings.TrimSpace(line)
+
+	args := []string{}
+
+	buf := ""
+
+	var escaped, double_quoted, single_quoted bool
+
+	for _, r := range line {
+		if escaped {
+			buf += string(r)
+			escaped = false
+			continue
+		}
+
+		if r == '\\' {
+			if single_quoted {
+				buf += string(r)
+			} else {
+				escaped = true
+			}
+			continue
+		}
+
+        if isSpace(r) {
+			if single_quoted || double_quoted {
+				buf += string(r)
+			} else {
+				args = append(args, buf)
+				buf = ""
+            }
+			continue
+        }
+
+		if r == '"' {
+			if single_quoted {
+				buf += string(r)
+			} else  {
+				double_quoted = !double_quoted
+			}
+			continue
+		}
+
+		if r == '\'' {
+			if double_quoted {
+				buf += string(r)
+				continue
+			} else {
+				single_quoted = !single_quoted
+			}
+			continue
+		}
+
+		buf += string(r)
+    }
+
+	if buf != "" {
+		args = append(args, buf)
+	}
+
+	if escaped || single_quoted || double_quoted {
+		return nil, errors.New("invalid command line string")
+	}
+
+	return args, nil
+}
---
+++ shellwords_test.go
@@ -0,0 +1,31 @@
+package shellwords
+
+import (
+	"reflect"
+	"testing"
+)
+
+var testcases = []struct {
+	line string
+	expected []string
+} {
+	{ `var --bar=baz`, []string{`var`, `--bar=baz`} },
+	{ `var --bar="baz"`, []string{`var`, `--bar=baz`} },
+	{ `var "--bar=baz"`, []string{`var`, `--bar=baz`} },
+	{ `var "--bar='baz'"`, []string{`var`, `--bar='baz'`} },
+	{ `var "--bar=\"baz'"`, []string{`var`, `--bar="baz'`} },
+	{ `var "--bar baz"`, []string{`var`, `--bar baz`} },
+	{ `var --"bar baz"`, []string{`var`, `--bar baz`} },
+}
+
+func TestSimple(t *testing.T) {
+	for _, testcase := range testcases {
+		args, err := Parse(testcase.line)
+		if err != nil {
+			t.Fatalf(err.Error())
+		}
+		if !reflect.DeepEqual(args, testcase.expected) {
+			t.Fatalf("Expected %v, but %v:", testcase.expected, args)
+		}
+	}
+}
