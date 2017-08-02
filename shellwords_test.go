253c54f03e167eda21ecbb2b6658693ddf651d33
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2017-08-03 01:05:39 +0900

    add test

--- shellwords_test.go
+++ shellwords_test.go
@@ -99,6 +99,14 @@ func TestBacktick(t *testing.T) {
 	if !reflect.DeepEqual(args, expected) {
 		t.Fatalf("Expected %#v, but %#v:", expected, args)
 	}
+	args, err = parser.Parse("echo $(`echo1)")
+	if err != nil {
+		t.Fatal(err)
+	}
+	expected = []string{"echo", "$(`echo1)"}
+	if !reflect.DeepEqual(args, expected) {
+		t.Fatalf("Expected %#v, but %#v:", expected, args)
+	}
 }
 
 func TestBacktickError(t *testing.T) {
